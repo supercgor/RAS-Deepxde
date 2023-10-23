880ec2e94876f942d76a325bf66c049174a479f7
Season | Dilu <siumabon123@gmail.com>
2023-10-23 02:08:31 -0400

    update

--- dde/deepxde/backend/pytorch/tensor.py
+++ dde/deepxde/backend/pytorch/tensor.py
@@ -16,8 +16,7 @@ if Version(torch.__version__) < Version("1.9.0"):
 # An alternative way is to use GPU by default if GPU is available, which is similar to
 # TensorFlow.
 if torch.cuda.is_available():
-    torch.set_default_tensor_type(torch.cuda.FloatTensor)
-
+    torch.set_default_device(torch.device("cuda"))
 
 lib = torch
 
